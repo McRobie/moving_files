02d17e0b339b0261f1b41631da89222de0e1ba27
McRobie <ryancmcrobie@gmail.com>
2017-03-30 11:41:51 -0500

    Update mv_and_crt.py

--- mv_and_crt.py
+++ mv_and_crt.py
@@ -1,6 +1,5 @@
 """ 
 author: Ryan McRobie
-
 This script was created to make directories based off of file names.
 Then move those files into the corresponding directory based off of their file name.
 """
@@ -9,46 +8,24 @@ import os
 import shutil
 
 # place your absolute file path here.
-working_directory = "absolute file path goes here."
+working_directory = "/mnt/c/Users/rmcrobie/Desktop/test"
 
 os.chdir(working_directory)
 contents = os.listdir(os.getcwd())
 
-true_files = []
-files = []
-dirs = []
-
-def create_files_list(contents):
+def creating(contents):
 	for item in contents:
-		if os.path.isfile(item) == True:
-			true_files.append(item)
-			parsed_item = item[5:]
-			files.append(parsed_item)
+		if os.path.isfile(item):
+			is_dir(item[13:])
+			move(item)
 
-def make_dirs_of_files(files):
-	for item in files:
-		if os.path.isdir(item) == False:
-			os.mkdir("./%s" % item)
+def is_dir(ext):
+	if os.path.isdir(ext) == False:
+		os.mkdir("./%s" % ext)
 
-def get_dirs_list(contents):
-	for item in contents:
-		if os.path.isdir(item):
-			dirs.append(item)
+def move(file):
+	dir = file[13:]	
+	shutil.move('./%s' % (file),'./%s/%s' % (dir,file))
+				
 
-def move_files():
-	for file in files:
-		pass
-
-def get_listing(files, dirs):
-	for fi, di in zip(true_files, dirs):
-		if fi[5:] in dirs:
-			shutil.move('./%s' % (fi),'./%s/%s' % (di,fi))
-
-
-create_files_list(contents)
-
-get_dirs_list(contents)
-
-make_dirs_of_files(files)
-
-get_listing(files, dirs)
+creating(contents)
